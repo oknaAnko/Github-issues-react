a1f1958648231e12a07fb8b9f8257fc589d0213a
Anko <anna.jedrzejko@op.pl>
2022-07-14 20:14:57 +0200

    feat: Store outline

---
+++ src/store/store.ts
@@ -0,0 +1,12 @@
+import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { resultsReducer } from "./results/reducer";
+
+export const store = configureStore({
+  reducer: {
+    results: resultsReducer,
+  },
+});
+
+export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
