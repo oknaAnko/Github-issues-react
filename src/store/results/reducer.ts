8486ab3eb4d6adc0f13376895d5fd8a9f0984555
Anko <anna.jedrzejko@op.pl>
2022-07-17 17:23:53 +0200

    Extend reducers with cases

--- src/store/results/reducer.ts
+++ src/store/results/reducer.ts
@@ -28,11 +28,22 @@ export const initialRepositoriestState: IRepositoriesState = {
 
 export const usersReducer = (state: IUsersState = initialUserstState, action: AnyAction) => {
   switch (action.type) {
-    case FETCH_USERS:
-      // console.log(action.payload);
+    case "FETCH_USERS/pending":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "FETCH_USERS/fulfilled":
+      console.log(action.payload);
       return {
         ...state,
         users: action.payload || [],
+      };
+    case "FETCH_USERS/rejected":
+      console.log(action.payload);
+      return {
+        ...state,
+        error: action.payload,
       };
     default:
       return state;
@@ -41,11 +52,22 @@ export const usersReducer = (state: IUsersState = initialUserstState, action: An
 
 export const repositoriesReducer = (state: IRepositoriesState = initialRepositoriestState, action: AnyAction) => {
   switch (action.type) {
-    case FETCH_REPOSITORIES:
-      // console.log(action.payload);
+    case "FETCH_REPOSITORIES/pending":
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case "FETCH_REPOSITORIES/fulfilled":
+      console.log(action.payload);
       return {
         ...state,
         repositories: action.payload || [],
+      };
+    case "FETCH_REPOSITORIES/rejected":
+      console.log(action.payload);
+      return {
+        ...state,
+        error: action.payload,
       };
     default:
       return state;
