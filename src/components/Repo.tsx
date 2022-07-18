d681c598f214f866a65477e5e59af337e787552d
Anko <anna.jedrzejko@op.pl>
2022-07-18 20:25:17 +0200

    Implement github colors for languages

--- src/components/Repo.tsx
+++ src/components/Repo.tsx
@@ -1,15 +1,21 @@
 import React from "react";
 import { IRepo } from "../helpers/interfaces";
+import { githubColors } from "../files/github-colors";
 
 interface Props {
   repo: IRepo;
 }
 
-const Repo: React.FC<Props> = (props) => {
-  console.log(props);
-  console.log(props.repo.full_name);
-
-  return <>{props.repo.full_name}</>;
+const Repo: React.FC<Props> = ({ repo }) => {
+  return (
+    <>
+      <p>
+        <span className="language-color" style={{ backgroundColor: githubColors[repo.language] }}></span>
+        {repo.language}
+      </p>
+      <p>{repo.full_name}</p>
+    </>
+  );
 };
 
 export default Repo;
