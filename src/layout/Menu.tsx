08b6e88bca7c88eeea9dcf0efd5451342e596778
Anko <anna.jedrzejko@op.pl>
2022-07-19 20:01:54 +0200

    Menu component structure

--- src/layout/Menu.tsx
+++ src/layout/Menu.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import logo from "../images/logo.png";
 
 const Menu = () => {
-  return <div>Menu</div>;
+  return (
+    <>
+      <header className="menu">
+        <div className="wrapper">
+          <img src={`${logo}`} alt="logo" />
+          <input type="text" className="button-search" placeholder="Search" />
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Menu;
